f8d2aa5f5039fdcbd4a1af622677de61165e8f1b
liaoxuan6 <2583771733@qq.com>
2021-07-16 14:05:07 +0800

    '更新gitignore‘

--- manage.py
+++ manage.py
@@ -7,7 +7,7 @@ from dao import models  # 模型文件必须导入进来，否则运行报错
 manager = Manager(app)
 Migrate(app=app, db=db)
 manager.add_command('db', MigrateCommand) # 创建数据库映射命令
-manager.add_command('start', Server(port=8000, use_debugger=True)) # 创建启动命令
+#manager.add_command('start', Server(port=8000, use_debugger=True)) # 创建启动命令
 
 if __name__ == '__main__':
     manager.run()
